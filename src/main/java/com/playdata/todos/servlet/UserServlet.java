96cfdb1d00cf294823a0300ec0fa86dd1c072fce
isnanaOne <1onlyonehana@gmail.com>
2023-06-15 10:48:34 +0900

    user post

--- src/main/java/com/playdata/todos/servlet/UserServlet.java
+++ src/main/java/com/playdata/todos/servlet/UserServlet.java
@@ -10,13 +10,20 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
         req.getRequestDispatcher("views/user.html").forward(req, resp);
-        super.doGet(req, resp);
-
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(name);
+        resp.setStatus(201);
+        resp.sendRedirect("/user"); // sendRedirect :이 페이지 자체를 넘겨버리는 것
+  
     }
 }
